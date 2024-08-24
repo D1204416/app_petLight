19e47503b6c369e893024eef63d6aeffda14be46
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-08-24 16:22:58 +0800

    update

--- app/src/main/java/com/example/petlight/MainActivity.java
+++ app/src/main/java/com/example/petlight/MainActivity.java
@@ -25,9 +25,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    private ArrayList<Todo> todoArrayList = new ArrayList<Todo>();
-    private ActivityResultLauncher<Intent> intentActivityResultLanucher;
+public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,125 +40,29 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             return insets;
         });
 
-        intentActivityResultLanucher = registerForActivityResult(
+        intentActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
-                    public void onActivityResult(ActivityResult o) {
-                        if(o.getData() != null && o.getResultCode() == Activity.RESULT_OK){
-                            String action = o.getData().getStringExtra("ACTION");
+                    public void onActivityResult(ActivityResult result) {
+                        // 寫另一個Activity回傳後, 得到回傳的資料之後的方法
+                        if (result.getData() != null && result.getResultCode() == Activity.RESULT_OK) {
+//                            bmi = result.getData().getDoubleExtra("BMI", -1);
 
-                            if(action.equals("new")){
-                                // 處理新增待辦事項
-                                String newTitle = o.getData().getStringExtra("TITLE");
-                                String newContent = o.getData().getStringExtra("CONTENT");
-                                int newNum = o.getData().getIntExtra("NUM", 0); // 獲取數量，預設為 0
-                                String imgName = o.getData().getStringExtra("IMG_NAME"); // 獲取圖片名稱
 
-                                Todo newData = new Todo(newTitle, newContent, newNum, imgName);
-                                todoArrayList.add(newData);
-                            } else if (action.equals("edit")) {
-                                // 處理編輯待辦事項
-                                String index = o.getData().getStringExtra("INDEX");
-                                String newTitle = o.getData().getStringExtra("TITLE");
-                                String newContent = o.getData().getStringExtra("CONTENT");
-                                int newNum = o.getData().getIntExtra("NUM", 0); // 獲取數量，預設為 0
-                                String imgName = o.getData().getStringExtra("IMG_NAME"); // 獲取圖片名稱
-
-                                Todo existingTodo = todoArrayList.get(Integer.parseInt(index));
-                                existingTodo.setTitle(newTitle);
-                                existingTodo.setContent(newContent);
-                                existingTodo.setNum(newNum); // 更新數量
-                                existingTodo.setImgName(imgName); // 更新圖片名稱
-                            } else{
-                                // 處理刪除待辦事項
-                                int removeIndex = o.getData().getIntExtra("INDEX", 0);
-                                todoArrayList.remove(removeIndex);
-                            }
                         }
-                        refreshPartListView(MainActivity.this);
                     }
                 }
         );
-
-        refreshPartListView(this);
     }
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-        refreshPartListView(this);
+
+    public void Goto(View view) {
+        Intent intent = new Intent(this, DisplayActivity.class);
+
+        intentActivityResultLauncher.launch(intent);
+
     }
 
-    public void newButtonClick(View view){
-        Intent intent = new Intent(this, TodoActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("ACTION", "new");
-        intent.putExtras(bundle);
-        intentActivityResultLanucher.launch(intent);
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Todo item = todoArrayList.get(i);
-        String title = item.getTitle();
-        String content = item.getContent();
-        int num = item.getNum(); // 獲取當前項目的數量
-
-        Intent intent = new Intent(this, TodoActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("ACTION", "edit");
-        bundle.putString("TITLE", title);
-        bundle.putString("CONTENT", content);
-        bundle.putInt("NUM", num); // 將數量傳遞到編輯活動
-        bundle.putInt("INDEX", i);
-
-        intent.putExtras(bundle);
-        intentActivityResultLanucher.launch(intent);
-    }
-
-    public void refreshPartListView(Context context) {
-        ListView partListView = (ListView) findViewById(R.id.todoListView);
-
-        // 判斷 ArrayList 是否為空
-        if (todoArrayList.isEmpty()) {
-            // 當 ArrayList 為空時，顯示提示訊息
-            Toast.makeText(context, "目前沒有任何資料", Toast.LENGTH_SHORT).show();
-            ArrayList<String> empty = new ArrayList<String>();
-            empty.add("目前無任何料件！");
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, empty);
-            partListView.setAdapter(adapter);
-        } else {
-            TodoAdapter adapter = new TodoAdapter(this, todoArrayList);
-            partListView.setAdapter(adapter);
-            partListView.setOnItemClickListener(this);
-
-            // 當 ArrayList 不為空時，設定 ListView 的長按事件來進行刪除
-            partListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> adapterView, View view, int itemIndex, long l) {
-                    AlertDialog.Builder dialog = new AlertDialog.Builder(context);
-                    String titleText = "確定刪除？";
-                    String contentText = "確定刪除這個項目？";
-                    dialog.setTitle(titleText);
-                    dialog.setMessage(contentText);
-                    dialog.setCancelable(true);
-
-                    // 設定 "確定" 按鈕的事件
-                    dialog.setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            todoArrayList.remove(itemIndex);
-                            refreshPartListView(context);  // 刷新 ListView
-                        }
-                    });
-
-                    // 設定 "取消" 按鈕的事件
-                    dialog.setNeutralButton("取消", null);
-                    dialog.show();
-                    return true;
-                }
-            });
-        }
-    }
 }
+
